e23a5788bf4c4b352a258afd2b82fa9d7faa54f8
virajchandra51 <virajchandra51@gmail.com>
2022-04-08 21:31:19 +0530

    more codes

---
+++ B_Array_Cloning_Technique.cpp
@@ -0,0 +1,100 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio() ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+
+#define PI (3.141592653589)
+#define M 1000000007
+#define pb push_back
+#define mp make_pair
+#define f first
+#define s second
+#define foreach(i, j, k, in) for(int i=j;i<k;i+=in)
+#define rforeach(i, j, k, in) for(int i=j;i>=k;i-=in)
+#define rep(i,j) foreach(i,0,j,1)
+#define rrep(i,j) rforeach(i,j,0,1)
+#define set_bits __builtin_popcountll
+#define all(x) x.begin(), x.end()
+#define py cout<<"YES"<<endl
+#define pn cout<<"NO"<<endl
+#define ps(x,y) fixed<<setprecision(y)<<x
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef map<int,int> mp;
+typedef set<int> st;
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr<<#x<<" ";_print(x); cerr<<endl;
+#else
+#define debug(x)
+#endif
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(double t) {cerr << t;}
+
+void solve()
+{
+    int n;
+    cin>>n;
+    int a[n];
+    rep(i,n)
+    cin>>a[i];
+    map<int,int> mpp;
+    rep(i,n)
+    {
+        mpp[a[i]]++;
+    }
+    int f=-1;
+    for(auto it:mpp)
+    {
+        if(it.s>f)
+        f=it.s;
+    }
+    int count=0; int add=f;
+    int s=n-f;
+    while(s>0)
+    {
+        count++;
+        if(add>s)
+        {
+            count+= (s);break;
+        }
+        count=count+add;
+        s=s-add;
+        add=add*2;
+    }
+
+    cout<<count<<endl;
+    
+}
+
+
+int32_t main()
+{
+    fastio()
+    #ifndef ONLINE_JUDGE
+        freopen("Error.txt","w",stderr);
+    #endif
+    //Rating? Neh. In love with experience.
+    //Code Karlo, Coz KHNH :)
+    int t;
+    cin>>t;
+    while(t--)
+    {
+    solve();
+    }
+    return 0;
+}
--- C_Madoka_and_Childish_Pranks.cpp
+++ C_Madoka_and_Childish_Pranks.cpp
@@ -37,9 +37,6 @@ void solve()
         cout<<"-1"<<endl;
         return;
     }
-
-    
-
 }
 
 
