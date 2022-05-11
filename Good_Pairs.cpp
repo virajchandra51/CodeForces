503e005cdc18796beb78394df823d84e971d74a4
Viraj Chandra <virajchandra51@gmail.com>
2022-05-12 02:02:37 +0530

    more codes

---
+++ Good_Pairs.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//Speed
+#define fastio() ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+
+//Macros
+#define rep(i,j) for(int i=0;i<j;i++)
+#define rrep(i,j) for(int i=j-1;i>=0;i--)
+#define all(x) x.begin(), x.end()
+
+//Typedef
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef vector<int> vi;
+typedef map<int,int> mii;
+
+void answertoProb()
+{
+    int n;
+    cin>>n;
+    int a[n];
+    int b[n];
+    rep(i,n)
+    cin>>a[i];
+    rep(i,n)
+    cin>>b[i];
+    map<pair<int,int>,int> mp;
+    long long ans = 0;
+    for(int i=0;i<n;i++){
+        ans += mp[{a[i],b[i]}];
+        mp[{b[i],a[i]}]++;
+    }
+    cout<<ans<<'\n';
+}
+
+
+int32_t main()
+{
+    fastio()
+    int t;
+    cin>>t;
+    while(t--)
+    {
+    answertoProb();
+    }
+    return 0;
+}
