edffe5fa852234e142c4581eb13fb136f517c45b
Viraj Chandra <virajchandra51@gmail.com>
2022-06-26 03:55:28 +0530

    more codes

---
+++ Fitness.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//Speed
+#define fastio() ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+
+//Macros
+#define rep(i,j) for(int i=0;i<j;i++)
+#define rrep(i,j) for(int i=j-1;i>=0;i--)
+#define all(x) x.begin(), x.end()
+
+//Typedef
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef vector<int> vi;
+typedef map<int,int> mii;
+
+void answertoProb()
+{
+    int x;
+    cin>>x;
+    cout<<x*10<<endl;
+}
+
+
+int32_t main()
+{
+    fastio()
+    int t;
+    cin>>t;
+    while(t--)
+    {
+    answertoProb();
+    }
+    return 0;
+}
---
+++ Make_all_equal_using_Pairs.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//Speed
+#define fastio() ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+
+//Macros
+#define rep(i,j) for(int i=0;i<j;i++)
+#define rrep(i,j) for(int i=j-1;i>=0;i--)
+#define all(x) x.begin(), x.end()
+
+//Typedef
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef vector<int> vi;
+typedef map<int,int> mii;
+
+void answertoProb()
+{
+    int n;
+    cin>>n;
+    mii mp;
+    rep(i,n)
+    {
+        int x;
+        cin>>x;
+        mp[x]++;
+    }
+    int ans=-1;
+    for(auto it: mp)
+    {
+        ans=max(ans,it.second);
+    }
+    cout<<n-ans<<endl;
+}
+
+
+int32_t main()
+{
+    fastio()
+    int t;
+    cin>>t;
+    while(t--)
+    {
+    answertoProb();
+    }
+    return 0;
+}
