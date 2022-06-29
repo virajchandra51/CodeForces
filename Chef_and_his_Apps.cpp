43be1b13cf04fa1d956d5f1b11305d6bd161b68c
Viraj Chandra <virajchandra51@gmail.com>
2022-06-30 02:34:02 +0530

    more codes

---
+++ Chef_and_his_Apps.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//Speed
+#define fastio() ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+
+//Macros
+#define rep(i,j) for(int i=0;i<j;i++)
+#define rrep(i,j) for(int i=j-1;i>=0;i--)
+#define all(x) x.begin(), x.end()
+
+//Typedef
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef vector<int> vi;
+typedef map<int,int> mii;
+
+void answertoProb()
+{
+    int s,x,y,z;
+    cin >> s >> x >> y >> z;
+    if(s-x-y>=z)
+    cout<<0<<endl;
+    else if(s-x>=z || s-y>=z)
+    cout<<1<<endl;
+    else
+    cout<<2<<endl;
+}
+
+
+int32_t main()
+{
+    fastio()
+    int t;
+    cin>>t;
+    while(t--)
+    {
+    answertoProb();
+    }
+    return 0;
+}
