d1eb25b376f6cc4ea72ff83425b71d6b78663e95
Viraj Chandra <virajchandra51@gmail.com>
2022-10-06 23:00:12 +0530

    more codes

--- yo2.cpp
+++ yo2.cpp
@@ -1,127 +1,107 @@
-// 2022-09-16 22:28:09
-// Viraj Chandra
-// Linkedin: https://www.linkedin.com/in/viraj-chandra-4073a8223/
-// Codeforces: https://codeforces.com/profile/khnhcodingkarlo
-// Codechef: https://www.codechef.com/users/codecozkhnhs
-
+/** author: kicker
+*   created: 05-10-2022 20:38
+**/
 #include <bits/stdc++.h>
 using namespace std;
+typedef long long                           ll;
+#define line                                << "\n"
+#define sep                                 << " " <<
+#define sp                                  << " "
+#define loop(i, x, n)                       for (ll i = x; i < n; i++)
+#define rep(i, x, n)                        for (int i = x; i < n; i++)
+#define rrep(i, x, n)                       for (ll i = x; i >= n; i--)
+#define f                                   first
+#define s                                   second
+#define all(x)                              x.begin(), x.end()
+#define dsort(x)                            sort(all(x), greater<ll>())
+#define vi                                  vector<int>
+#define vs                                  vector<string>
+#define vll                                 vector<long long>
+#define pb                                  push_back
+#define pf                                  push_front
+#define vc                                  vector<char>
+#define sll                                 set<ll>
+#define mll                                 map<ll, ll>
+#define pqMax                               priority_queue<ll>
+#define pqMin                               priority_queue<int, vector<ll>, greater<ll>>
 
-//Speed
-#define fastio() ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+template <typename T> bool _equal(T a[], ll n) { return std::all_of(a, a+n, [a](T x){ return x==a[0]; }); }
+template <typename T> T aMax(T a[], T n) { T mx = a[0]; loop(i, 0, n) mx = max(mx, a[i]); return mx; }
+template <typename T> T aMin(T a[], T n) { T mn = a[0]; loop(i, 0, n) mn = min(mn, a[i]); return mn; }
+template <typename T> void parr(T a[], ll n) { loop(i, 0, n) cout << a[i] << " "; cout line; }
+template <typename T> void print(vector<T> v) { loop(i, 0, v.size()) cout << v[i] << " "; cout line; }
 
-//Macros
-#define IOtext freopen("input.txt","r",stdin); freopen("output.txt","w",stdout);
-#define PI (3.141592653589)
-#define M 1000000007
-#define pb push_back
-#define f first
-#define s second
-#define foreach(i, j, k, in) for(int i=j;i<k;i+=in)
-#define rforeach(i, j, k, in) for(int i=j;i>=k;i-=in)
-#define rep(i,j) foreach(i,0,j,1)
-#define rrep(i,j) rforeach(i,j,0,1)
-#define set_bits(x) __builtin_popcountll(x)
-#define zero_bits(x) __builtin_ctzll(x)
-#define sz(s) (int)(s.size())
-#define Num_of_Digits(n) ((int)log10(n) + 1)
-#define inint(x) int x; cin>>x;
-#define inll(x) long long int x; cin>>x;
-#define instr(x) string x; cin>>x;
-#define all(x) x.begin(), x.end()
-#define py cout<<"YES"<<endl
-#define pn cout<<"NO"<<endl
-#define pm cout<<"-1"<<endl
-#define ps(x,y) fixed<<setprecision(y)<<x
-
-//Typedef
-typedef long long ll;
-typedef unsigned long long ull;
-typedef long double lld;
-typedef pair<int, int> pi;
-typedef pair<ll, ll> pl;
-typedef vector<int> vi;
-typedef vector<ll> vl;
-typedef vector<pi> vpi;
-typedef vector<pl> vpl;
-typedef vector<vi> vvi;
-typedef vector<vl> vvl;
-typedef map<int,int> mii;
-typedef set<int> st;
-typedef unordered_map<int,int> ump;
-
-#ifndef ONLINE_JUDGE
-#define debug(x) cerr<<#x<<" ";_print(x); cerr<<endl;
-#else
-#define debug(x)
-#endif
-
-void _print(ll t) {cerr << t;}
-void _print(int t) {cerr << t;}
-void _print(string t) {cerr << t;}
-void _print(char t) {cerr << t;}
-void _print(double t) {cerr << t;}
-
-// Operator overloads
-template<typename T> // cin >> vector<T>
-istream& operator>>(istream &istream, vector<T> &v){for (auto &it : v)cin >> it;return istream;}
-template<typename T> // cout << vector<T>
-ostream& operator<<(ostream &ostream, const vector<T> &c) { for (auto &it : c) cout << it << " "; return ostream; }
-template<typename T, typename V> // cin >> map<T,T>
-istream& operator<<(istream &istream, const map<T,V> &c) { for (auto &it : c) cout >> it.first>> it.second; return istream; }
-template<typename T, typename V> // cout << map<T,T>
-ostream& operator<<(ostream &ostream, const map<T,V> &c) { for (auto &it : c) cout << it.first << " " << it.second<<endl; return ostream; }
-
-//Sorting
-bool sorta(const pair<int,int> &a,const pair<int,int> &b){return (a.second < b.second);}
-bool sortd(const pair<int,int> &a,const pair<int,int> &b){return (a.second > b.second);}
-
-//Bits
-string decToBinary(int n){string s="";int i = 0;while (n > 0) {s =to_string(n % 2)+s;n = n / 2;i++;}return s;}
-ll binaryToDecimal(string n){string num = n;ll dec_value = 0;int base = 1;int len = num.length();for(int i = len - 1; i >= 0; i--){if (num[i] == '1')dec_value += base;base = base * 2;}return dec_value;}
-
-//Check
-bool isPrime(ll n){if(n<=1)return false;if(n<=3)return true;if(n%2==0||n%3==0)return false;for(int i=5;i*i<=n;i=i+6)if(n%i==0||n%(i+2)==0)return false;return true;}
-bool isPowerOfTwo(int n){if(n==0)return false;return (ceil(log2(n)) == floor(log2(n)));}
-bool isPerfectSquare(ll x){if (x >= 0) {ll sr = sqrt(x);return (sr * sr == x);}return false;}
-
-//Constants
-vector <ll> primes;
-vector <bool> is_prime;
-
-// Mathematical functions
-void Sieve(int n){ is_prime.assign(n + 1, true); is_prime[0] = is_prime[1] = false; for(ll i = 2; i * i <= n; i++) if(is_prime[i]) for(ll j = i * i; j <= n; j += i) is_prime[j] = false;}
-void get_primes(int n){ for(int i = 2; i <= n; i++)  if(is_prime[i])  primes.push_back(i); }
-ll mod_add(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a + b) % m) + m) % m;}
-ll mod_sub(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a - b) % m) + m) % m;}
-ll gcd(ll a, ll b){if (b == 0)return a;return gcd(b, a % b);} //__gcd 
-ll lcm(ll a, ll b){return (a/gcd(a,b)*b);}
-ll moduloMultiplication(ll a,ll b,ll mod){ll res = 0;a %= mod;while (b){if (b & 1)res = (res + a) % mod;b >>= 1;}return res;}
-ll powermod(ll x, ll y, ll p){ll res = 1;x = x % p;if (x == 0) return 0;while (y > 0){if (y & 1)res = (res*x) % p;y = y>>1;x = (x*x) % p;}return res;}
-
-void solve()
+ll kadane(ll a[], ll s, ll e)
 {
-    int n;
-    cin>>n;
-    vi a;
-    cin>>a;
-    cout<<a<<endl;
+    ll max_so_far = INT_MIN, max_ending_here = 0;
+ 
+    for (ll i = s; i <= e; i++) {
+        max_ending_here = max_ending_here + a[i];
+        if (max_so_far < max_ending_here)
+            max_so_far = max_ending_here;
+ 
+        if (max_ending_here < 0)
+            max_ending_here = 0;
+    }
+    return max_so_far;
 }
 
+void solve() {
+    ll n;
+    cin >> n;
+    ll a[n];
+    loop(i, 0, n) cin >> a[i];
 
-int32_t main()
-{
-    fastio()
-    #ifndef ONLINE_JUDGE
-        freopen("Error.txt","w",stderr);
-    #endif
-    //Rating? Neh. In love with experience.
-    //Code Karlo, Coz KHNH :)
-    int t;
-    cin>>t;
-    while(t--)
-    {
-    solve();
+    ll m;
+    cin >> m;
+    ll b[m];
+    loop(i, 0, m) cin >> b[i];
+
+    ll fs = 0, ls = 0, end = 0, fneg = 0, lneg = 0;
+    loop(i, 0, n) {
+        end += a[i];
+        if (end < 0) {
+            fneg = i;
+            break;
+        }
+        if (end > fs) {
+            fs = end;
+        }
     }
+
+    end = 0;
+    rrep(i, n - 1, 0) {
+        end += a[i];
+        if (end < 0) {
+            lneg = i;
+            break;
+        }
+        if (end > ls) {
+            ls = end;
+        }
+    }
+
+    ll ans = max(fs, ls);
+    cout<<ans<<endl;
+    loop (i, 0, m) {
+        if (b[i] > 0) ans += b[i];
+    }
+    ans = max(ans, kadane(a, fneg, lneg));
+    cout << ans line;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);cin.tie(NULL);
+
+    #ifndef ONLINE_JUDGE
+    #endif
+
+    ll t = 1;
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+
+    cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << endl;
     return 0;
 }
