419fb37b3f326457f2a253115522f04003544c0a
Viraj Chandra <virajchandra51@gmail.com>
2023-01-26 03:34:11 +0530

    more codes

---
+++ yo2.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    cout<<10000<<endl;
+    for(int i=1;i<=10000;i++)
+    cout<<1<<" "<<100000000<<" "<<100000000<<" "<<100000000<<endl;
+    return 0;
+}
