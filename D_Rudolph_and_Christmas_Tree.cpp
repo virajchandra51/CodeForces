ba9828a2ed54f256f4fcc7e9e234cc4c3f97a479
Viraj Chandra <virajchandra51@gmail.com>
2023-07-08 01:18:43 +0530

    more codes

---
+++ D_Rudolph_and_Christmas_Tree.cpp
@@ -0,0 +1,142 @@
+// 2023-07-07 23:44:07
+// Viraj Chandra
+// Linkedin: https://www.linkedin.com/in/viraj-chandra-4073a8223/
+// Codeforces: https://codeforces.com/profile/khnhcodingkarlo
+// Codechef: https://www.codechef.com/users/codecozkhnhs
+
+// CPP Program to demonstrate Big Integer data type
+#include <bits/stdc++.h>
+#include <iomanip>
+
+using namespace std;
+
+//Speed
+#define fastio() ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+
+//Macros
+#define IOtext freopen("input.txt","r",stdin); freopen("output.txt","w",stdout);
+#define PI (3.141592653589)
+#define M 1000000007
+#define pb push_back
+#define f first
+#define s second
+#define foreach(i, j, k, in) for(int i=j;i<k;i+=in)
+#define rforeach(i, j, k, in) for(int i=j;i>=k;i-=in)
+#define rep(i,j) foreach(i,0,j,1)
+#define rrep(i,j) rforeach(i,j,0,1)
+#define set_bits(x) __builtin_popcountll(x)
+#define zero_bits(x) __builtin_ctzll(x)
+#define sz(s) (int)(s.size())
+#define Num_of_Digits(n) ((int)log10(n) + 1)
+#define inint(x) int x; cin>>x;
+#define inll(x) long long int x; cin>>x;
+#define instr(x) string x; cin>>x;
+#define all(x) x.begin(), x.end()
+#define os(x) cout << x << " ";
+#define out(x) cout << x << endl;
+#define MAX(x) *max_element(all(x))
+#define MIN(x) *min_element(all(x))
+#define SUM(x) accumulate(all(x), 0LL)
+#define COUNT(x,u) count(all(x), u)
+#define B break
+#define C continue
+#define py cout<<"YES"<<endl
+#define pn cout<<"NO"<<endl
+#define pm cout<<"-1"<<endl
+#define ps(x,y) fixed<<setprecision(y)<<x
+
+//Typedef
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef map<int,int> mii;
+typedef set<int> st;
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr<<#x<<" ";_print(x); cerr<<endl;
+#else
+#define debug(x)
+#endif
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(double t) {cerr << t;}
+
+// Operator overloads
+template<typename T> // cin >> vector<T>
+istream& operator>>(istream &istream, vector<T> &v){for (auto &it : v)cin >> it;return istream;}
+template<typename T> // cout << vector<T>
+ostream& operator<<(ostream &ostream, const vector<T> &c) { for (auto &it : c) cout << it << " "; return ostream; }
+template<typename T, typename V> // cout << map<T,T>
+ostream& operator<<(ostream &ostream, const map<T,V> &c) { for (auto &it : c) cout << it.first << " " << it.second<<endl; return ostream; }
+
+//Sorting
+bool sorta(const pair<int,int> &a,const pair<int,int> &b){return (a.second < b.second);}
+bool sortd(const pair<int,int> &a,const pair<int,int> &b){return (a.second > b.second);}
+
+//Bits
+string decToBinary(int n){string s="";int i = 0;while (n > 0) {s =to_string(n % 2)+s;n = n / 2;i++;}return s;}
+ll binaryToDecimal(string n){string num = n;ll dec_value = 0;int base = 1;int len = num.length();for(int i = len - 1; i >= 0; i--){if (num[i] == '1')dec_value += base;base = base * 2;}return dec_value;}
+
+
+void solve()
+{
+    double n,b,h;
+    cin>>n>>b>>h;
+    n*=1.0;
+    b*=1.0;
+    h*=1.0;
+    vector<double> a(n);
+    cin>>a;
+    sort(all(a));
+    double ans = 0;
+    for(int i=0;i<n-1;i++)
+    {
+        if(a[i]+h<=a[i+1])
+        {
+            double area = b*h*1.0;
+            area /= 2.0;
+            ans += area;
+        }
+        else
+        {
+            double diffH = a[i+1]-a[i];
+            double y = (1.0*((diffH)*b))/(2.0*h);
+            double sideA = 1.0*(1.0*b-2.0*y);
+            double area = (double(sideA)*diffH)/2.0 + (double(b)*diffH)/2.0;;
+            ans += area;
+        }
+    }
+    double area = b*h*1.0;
+    area /= 2.0;
+    ans += area;
+    cout<<setprecision(15)<<ans<<endl;
+}
+
+
+int32_t main()
+{
+    fastio()
+    #ifndef ONLINE_JUDGE
+        freopen("Error.txt","w",stderr);
+    #endif
+    //Rating? Neh. In love with experience.
+    //Code Karlo, Coz KHNH :)
+    int t;
+    cin>>t;
+    while(t--)
+    {
+    solve();
+    }
+    return 0;
+}
