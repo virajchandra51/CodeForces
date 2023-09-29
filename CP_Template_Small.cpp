0b699089e99bb0d110d6ddb9a2ecb5526c87262e
Viraj Chandra <virajchandra51@gmail.com>
2023-09-29 21:42:21 +0530

    templates

---
+++ CP_Template_Small.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//Speed
+#define fastio() ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+
+//Macros
+#define rep(i,j) for(int i=0;i<j;i++)
+#define rrep(i,j) for(int i=j-1;i>=0;i--)
+#define all(x) x.begin(), x.end()
+
+//Typedef
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef vector<int> vi;
+typedef map<int,int> mii;
+
+void answertoProb()
+{
+
+}
+
+
+int32_t main()
+{
+    fastio()
+    int t;
+    cin>>t;
+    while(t--)
+    {
+    answertoProb();
+    }
+    return 0;
+}
