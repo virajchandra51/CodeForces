f3c5cb2c3e482a406643d7c3a4aa2229e36bed9f
Viraj Chandra <virajchandra51@gmail.com>
2023-10-04 12:14:28 +0530

    more codes

---
+++ Sports_Section.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x;
+    cin>>x;
+    if(x>=8) cout<<"YES"<<endl;
+    else cout<<"NO"<<endl;
+	// your code goes here
+	return 0;
+}
