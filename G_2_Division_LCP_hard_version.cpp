8cb7d8152f37204f03e13b554889fdad8f4a03a2
Viraj Chandra <virajchandra51@gmail.com>
2024-05-03 13:51:52 +0530

    more codes

--- G_2_Division_LCP_hard_version.cpp
+++ G_2_Division_LCP_hard_version.cpp
@@ -1,130 +1,57 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef long long ll;
-typedef vector<pair<ll, ll> > vpll;
-typedef pair<int, int> pii;
-typedef vector<pair<int, int> > vpii;
-typedef vector<ll> vl;
-typedef pair<ll, ll> pll;
-
-#define SZ 1000005
-
-pll p = {31, 53};
-pll inv_p = {129032259, 56603774};
-pll inv_p_1 = {233333335, 788461544};
-
-ll mod = 1e9 + 7;
-
-vpll hash_array1;
-vpll hash_array2;
-ll powerval[SZ];
-
-void calcpow()
-{
-    powerval[0] = 1;
-    for (int i = 1; i < SZ; i++)
-    {
-        powerval[i] = (p.first * powerval[i - 1]) % mod;
+vector<int> z_function(string s) {
+    int n = s.size(), l = 0, r = 0;
+    vector<int> z(n);
+    for(int i = 1; i < n; i++) {
+        if(i < r) z[i] = min(r - i, z[i - l]);
+        while(i + z[i] < n && s[z[i]] == s[i + z[i]]) z[i]++;
+        if(i + z[i] > r) l = i, r = i + z[i];
     }
+    z[0] = n;
+    return z;
 }
 
-vl calchash(string &s)
-{
-    int n = s.length();
-    vl hash_array(n);
-    for (int i = 0; i < n; i++)
-        hash_array[i] = 0;
-    hash_array[0] = (s[0] - 'a' + 1);
-    for (int i = 1; i < n; i++)
-    {
-        hash_array[i] = ((p.first * hash_array[i - 1]) + (s[i] - 'a' + 1)) % mod;
-    }
-    return hash_array;
-}
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
 
-ll hashval(ll l, ll r, vl &hash_array)
-{
-    ll len = r - l + 1;
-    int n = hash_array.size();
-    if (len <= 0 || l < 0 || r >= n || r < l)
-        return 0;
-    ll ans = hash_array[r];
-    if (l >= 1)
-    {
-        ans -= (hash_array[l - 1] * powerval[len]) % mod;
-    }
-    if (ans < 0)
-        ans += mod;
-    return ans;
-}
-
-vl hv;
-ll n, l, r;
-
-map<ll, ll> store;
-
-ll check(ll x)
-{
-    if (store.count(x))
-        return store[x];
-    ll ans = 0;
-    ll temp = hashval(0, x - 1, hv);
-
-    for (int i = 0; i < n; i++)
-    {
-        int j = i + x - 1;
-        if (j >= n)
-            break;
-
-        ll cur = hashval(i, j, hv);
-
-        if (cur == temp)
-        {
-            i += x - 1;
-            ans++;
-        }
-    }
-    return store[x] = ans;
-}
-
-int main()
-{
     int t;
     cin >> t;
-    calcpow();
-    while (t--)
-    {
-        cin >> n >> l >> r;
+
+    while (t--) {
+        int n, L, R;
+        cin >> n >> L >> R;
+
         string str;
         cin >> str;
-        store.clear();
-        hv.clear();
-        hv = calchash(str);
-        ll pre = n;
-        for (ll x = l; x <= r; x++)
-        {
-            ll low = 1;
-            ll high = pre;
-            ll ans = 0;
-            while (low <= high)
-            {
-                ll mid = (low + high) / 2;
-                if (check(mid) >= x)
-                {
-                    ans = mid;
-                    low = mid + 1;
-                }
-                else
-                    high = mid - 1;
-            } // O(nlogn)
-            cout << ans << " ";
-            pre = ans;
+
+        vector<int> z = z_function(str);
+        vector<int> ans(n + 1, 0);
+        set<int> st;
+
+        priority_queue<pair<int,int>> pq;
+        for (int i = 0; i < n; i++) pq.push({z[i], i});
+
+        for (int l = n; l >= 1; l--) {
+            while (!pq.empty() && pq.top().first == l) {
+                st.insert(pq.top().second);
+                pq.pop();
+            }
+            int i = 0;
+            while (st.lower_bound(i) != st.end()) {
+                ans[l]++;
+                i = *st.lower_bound(i) + l;
+            }
         }
-        cout << endl;
+
+        for (int k = L; k <= R; k++) {
+            int l = lower_bound(ans.begin() + 1, ans.end(), k, [&] (int mid, int target) {
+                return mid >= target;
+            }) - ans.begin() - 1;
+            cout << l << " \n"[k == R];
+        }
     }
+    
     return 0;
 }
-
-// tc - O(nlogn)
-// sc - O(n)
