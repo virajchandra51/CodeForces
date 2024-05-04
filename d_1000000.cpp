8ff9567cf5abbd9c6941e1529641f1619815f67f
Viraj Chandra <virajchandra51@gmail.com>
2024-05-04 23:50:51 +0530

    more codes

--- d_1000000.cpp
+++ d_1000000.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define pi (3.141592653589)
-#define mod 1e9+7
+#define mod 1e9 + 7
 #define ll long long int
 #define pb push_back
 #define mp make_pair
@@ -9,35 +9,37 @@ using namespace std;
 #define f first
 #define s second
 #define vi vector<int>
-#define rrep(i, n) for(int i=n-1;i>=0;i--)
-#define rep(i,n) for(int i=0;i<n;i++)
+#define rrep(i, n) for (int i = n - 1; i >= 0; i--)
+#define rep(i, n) for (int i = 0; i < n; i++)
 #define fast ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
 
 int32_t main()
 {
     fast
-    //Rating? Neh. In love with experience.
-    //Code Karlo, Coz KHNH :)
-    
-    int t; int i=1;
-    cin>>t;
-    while(t--)
+        // Rating? Neh. In love with experience.
+        // Code Karlo, Coz KHNH :)
+
+        int t;
+    int i = 1;
+    cin >> t;
+    while (t--)
     {
-    int n;
-    cin>>n;
-    int a[n];
-    rep(i,n)
-    cin>>a[i];
-    sort(a,a+n);
-    int counter=1;
-    rep(i,n)
-    {
-        if(a[i]>=counter)
-        counter++;
-    }
-    
-    cout<<"Case #"<<i<<": "<<counter-1<<endl;
-    i++;
+        int n;
+        cin >> n;
+        int a[n];
+        rep(i, n)
+                cin >>
+            a[i];
+        sort(a, a + n);
+        int counter = 1;
+        rep(i, n)
+        {
+            if (a[i] >= counter)
+                counter++;
+        }
+
+        cout << "Case #" << i << ": " << counter - 1 << endl;
+        i++;
     }
     return 0;
 }
