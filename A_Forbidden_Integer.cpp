4e91bd8fac75bb705e27de218912b66cde450a8d
Viraj Chandra <virajchandra51@gmail.com>
2024-05-07 01:43:51 +0530

    more codes

--- A_Forbidden_Integer.cpp
+++ A_Forbidden_Integer.cpp
@@ -1,24 +1,16 @@
-// 2024-05-07 01:12:13
+// 2022-06-28 20:02:48
 // Viraj Chandra
 // Linkedin: https://www.linkedin.com/in/viraj-chandra-4073a8223/
 // Codeforces: https://codeforces.com/profile/khnhcodingkarlo
 // Codechef: https://www.codechef.com/users/codecozkhnhs
 
 #include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-
 using namespace std;
-using namespace __gnu_pbds;
-
-//PBDS
-typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds; // find_by_order, order_of_key
 
 //Speed
 #define fastio() ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
 
 //Macros
-#define IOtext freopen("input.txt","r",stdin); freopen("output.txt","w",stdout);
 #define PI (3.141592653589)
 #define M 1000000007
 #define pb push_back
@@ -30,12 +22,10 @@ typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_
 #define rrep(i,j) rforeach(i,j,0,1)
 #define set_bits(x) __builtin_popcountll(x)
 #define zero_bits(x) __builtin_ctzll(x)
-#define Num_of_Digits(n) ((int)log10(n) + 1)
 #define inint(x) int x; cin>>x;
 #define inll(x) long long int x; cin>>x;
 #define instr(x) string x; cin>>x;
 #define all(x) x.begin(), x.end()
-#define out(x) cout << x << endl;
 #define py cout<<"YES"<<endl
 #define pn cout<<"NO"<<endl
 #define pm cout<<"-1"<<endl
@@ -43,6 +33,8 @@ typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_
 
 //Typedef
 typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
 typedef pair<int, int> pi;
 typedef pair<ll, ll> pl;
 typedef vector<int> vi;
@@ -52,19 +44,25 @@ typedef vector<pl> vpl;
 typedef vector<vi> vvi;
 typedef vector<vl> vvl;
 typedef map<int,int> mii;
-typedef map<ll,ll> mll;
-typedef map<char,int> mci;
 typedef set<int> st;
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr<<#x<<" ";_print(x); cerr<<endl;
+#else
+#define debug(x)
+#endif
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(double t) {cerr << t;}
 
 // Operator overloads
 template<typename T> // cin >> vector<T>
 istream& operator>>(istream &istream, vector<T> &v){for (auto &it : v)cin >> it;return istream;}
 template<typename T> // cout << vector<T>
 ostream& operator<<(ostream &ostream, const vector<T> &c) { for (auto &it : c) cout << it << " "; return ostream; }
-template<typename T, typename V> // cout << map<T,V>
-ostream& operator<<(ostream &ostream, const map<T,V> &c) { for (auto &it : c) cout << it.first << " " << it.second<<endl; return ostream; }
-template<typename T, typename V> // cout << vector<pair<T,V>>
-ostream& operator<<(ostream &ostream, const vector<pair<T,V>> &c) { for (auto &it : c) cout << it.first << " " << it.second<<endl; return ostream; }
 
 //Sorting
 bool sorta(const pair<int,int> &a,const pair<int,int> &b){return (a.second < b.second);}
@@ -79,75 +77,70 @@ bool isPrime(ll n){if(n<=1)return false;if(n<=3)return true;if(n%2==0||n%3==0)re
 bool isPowerOfTwo(int n){if(n==0)return false;return (ceil(log2(n)) == floor(log2(n)));}
 bool isPerfectSquare(ll x){if (x >= 0) {ll sr = sqrt(x);return (sr * sr == x);}return false;}
 
-//Constants
-vector <ll> primes;
-vector <bool> is_prime;
-
 // Mathematical functions
-void Sieve(int n){ is_prime.assign(n + 1, true); is_prime[0] = is_prime[1] = false; for(ll i = 2; i * i <= n; i++) if(is_prime[i]) for(ll j = i * i; j <= n; j += i) is_prime[j] = false;}
-void get_primes(int n){ for(int i = 2; i <= n; i++)  if(is_prime[i])  primes.push_back(i); }
-ll mod_add(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a + b) % m) + m) % m;}
-ll mod_sub(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a - b) % m) + m) % m;}
 ll gcd(ll a, ll b){if (b == 0)return a;return gcd(b, a % b);} //__gcd 
 ll lcm(ll a, ll b){return (a/gcd(a,b)*b);}
 ll moduloMultiplication(ll a,ll b,ll mod){ll res = 0;a %= mod;while (b){if (b & 1)res = (res + a) % mod;b >>= 1;}return res;}
 ll powermod(ll x, ll y, ll p){ll res = 1;x = x % p;if (x == 0) return 0;while (y > 0){if (y & 1)res = (res*x) % p;y = y>>1;x = (x*x) % p;}return res;}
-//To find modulo inverse, call powermod(A,M-2,M)
+
+void solve()
+{
+    ll n,k,x;
+    cin>>n>>k>>x;
+    if(x==1)
+    {
+        if(k==1)
+        {
+            pn;
+            return;
+        }
+        
+            if(n&1)
+            {
+                if(k<3)
+                pn;
+                else
+                {
+                    n-=3;
+                    py;
+                    cout<<n/2+1<<endl;
+                    rep(i,n/2) cout<<2<<" ";
+                    cout<<3<<" ";
+                    cout<<endl;
+                }
+            }
+            else
+            {
+                py;
+                cout<<n/2<<endl;
+                rep(i,n/2) cout<<2<<" ";
+                cout<<endl;
+            }
+        
+    }
+    else
+    {
+        py;
+        cout<<n<<endl;
+        rep(i,n) cout<<1<<" ";
+        cout<<endl;
+    }
+}
+
 
 int32_t main()
 {
     fastio()
+    #ifndef ONLINE_JUDGE
+        freopen("Error.txt","w",stderr);
+    #endif
     //Rating? Neh. In love with experience.
     //Code Karlo, Coz KHNH :)
-    auto solve = [&] () {
-        ll a,b,c;
-        cin>>a>>b>>c;
-        for(int i=0;i<=30;i++)
-        {
-            if(a==b && b==c)
-            {
-                cout<<"YES"<<endl;
-                return;
-            }
-            int cnt=0;
-            if(a&(1<<i))
-                cnt++;
-            if(b&(1<<i))
-                cnt++;
-            if(c&(1<<i))
-                cnt++;
-            if(cnt==0 || cnt==3)
-                break;
-            if(cnt==1)
-            {
-                if(a&(1<<i))   
-                    a=a+(1<<i);
-                else if(b&(1<<i))
-                    b=b+(1<<i);
-                else if(c&(1<<i))
-                    c=c+(1<<i);
-            }
-            else if(cnt==2)
-            {
-                if((a&(1<<i))==0)
-                    a=a+(1<<i);
-                else if((b&(1<<i))==0)
-                    b=b+(1<<i);
-                else if((c&(1<<i))==0)
-                    c=c+(1<<i);
-            }
-        }
-        if(a==b && b==c)
-            cout<<"YES"<<endl;
-        else
-            cout<<"NO"<<endl;
-    };
-
     int t;
     cin>>t;
     while(t--)
     {
-        solve();
+    solve();
     }
     return 0;
 }
