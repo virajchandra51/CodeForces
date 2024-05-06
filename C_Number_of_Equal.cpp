8d34b6fb1b65a00cd24901ece8446332d6b7c74b
Viraj Chandra <virajchandra51@gmail.com>
2024-05-07 00:08:56 +0530

    more codes

---
+++ C_Number_of_Equal.cpp
@@ -0,0 +1,30 @@
+#include "bits/stdc++.h"
+#define int long long
+using namespace std;
+signed main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int n, m, ans = 0;
+    cin >> n >> m;
+    vector<int> a(n), b(m);
+    for (int &x : a)
+        cin >> x;
+    for (int &x : b)
+        cin >> x;
+    for (int i = 0, j = 0, c = 0; i < n; i++)
+    {
+        if (i > 0 && a[i - 1] == a[i])
+        {
+            ans += c;
+            continue;
+        }
+        c = 0;
+        while (j < m && a[i] > b[j])
+            j++;
+        while (j < m && a[i] == b[j])
+            j++, c++;
+        ans += c;
+    }
+    cout << ans;
+}
