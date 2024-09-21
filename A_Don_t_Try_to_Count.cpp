acce47ad5a7775e81047f94b743fa4246fb368c0
Viraj Chandra <virajchandra51@gmail.com>
2024-09-21 16:11:18 +0530

    more codes

--- A_Don_t_Try_to_Count.cpp
+++ A_Don_t_Try_to_Count.cpp
@@ -7,15 +7,15 @@ bool check(string s, string x)
     {
         return false;
     }
-    for (int i = 0; i < x.size() - s.size() + 1; i++)
+    for (int i = 0; i < x.size() - s.size() + 1; i++) // n - m + 1
     {
-        if (x.substr(i, s.size()) == s)
+        if (x.substr(i, s.size()) == s) // m
         {
             return true;
         }
     }
     return false;
-}
+} // O(n*m)
 
 int main()
 {
@@ -29,13 +29,14 @@ int main()
         cin >> x;
         string s;
         cin >> s;
+        // inputs
 
-        string x0 = x;
-        string x1 = x0 + x0;
-        string x2 = x1 + x1;
-        string x3 = x2 + x2;
-        string x4 = x3 + x3;
-        string x5 = x4 + x4;
+        string x0 = x; //  0 operations
+        string x1 = x0 + x0; // 1 operation
+        string x2 = x1 + x1; // 2 operations
+        string x3 = x2 + x2; // 3 operations
+        string x4 = x3 + x3; // 4 operations
+        string x5 = x4 + x4; // 5 operations
 
         long long ans = -1;
         if (check(s, x0))
@@ -55,5 +56,5 @@ int main()
     return 0;
 }
 
-// tc - O(1)
+// tc - O(2^5*n*m) = O(32*n*m)
 // sc - O(1)
