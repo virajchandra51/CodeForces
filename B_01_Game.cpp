0d411153130e65ad1ea04795c7a23d114d4d9dfb
Viraj Chandra <virajchandra51@gmail.com>
2025-01-09 02:20:30 +0530

    more codes

--- B_01_Game.cpp
+++ B_01_Game.cpp
@@ -1,29 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void solve()
-{
-    string s;
-    cin>>s;
-    int one = 0;
-    int zero = 0;
-    for(int i = 0; i < s.length(); i++)
-    {
-        if(s[i]=='0') zero++;
-        else one++;
-    }
-    int m = min(zero, one);
-    if(m%2!=0) cout<<"DA"<<endl;
-    else cout<<"NET"<<endl;
-}
-
 int main()
 {
     int t;
     cin >> t;
     while (t--)
     {
-        solve();
+        string s;
+        cin >> s;
+
+        int count_of_one = 0;
+        int count_of_zero = 0;
+        for (int i = 0; i < s.length(); i++) // n
+        {
+            if (s[i] == '0')
+                count_of_zero++;
+            else
+                count_of_one++;
+        }
+
+        int operations = min(count_of_zero, count_of_one);
+        if (operations % 2 != 0)
+            cout << "DA" << endl;
+        else
+            cout << "NET" << endl;
     }
     return 0;
 }
+
+// tc - O(n) = O(100)
+// sc - O(n) = O(100)
