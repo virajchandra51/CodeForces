532ab3fd6a380b17a929d21f9502fc1078462d15
Viraj Chandra <virajchandra51@gmail.com>
2025-02-17 23:09:33 +0530

    more codes

--- D_Distinct_Split.cpp
+++ D_Distinct_Split.cpp
@@ -16,24 +16,27 @@ int main()
         vector<long long> prefix_unique_count(n + 1, 0);
         vector<long long> suffix_unique_count(n + 1, 0);
 
-        for (long long i = 1; i <= n; i++)
+        for (long long i = 1; i <= n; i++) // n
         {
-            st.insert(s[i - 1]);
-            prefix_unique_count[i] = st.size();
+            st.insert(s[i - 1]); // 1
+            prefix_unique_count[i] = st.size(); // 1
         }
 
         st.clear();
 
-        for (long long i = n; i >= 1; i--)
+        for (long long i = n; i >= 1; i--) // n
         {
-            st.insert(s[i - 1]);
-            suffix_unique_count[i] = st.size();
+            st.insert(s[i - 1]); // 1
+            suffix_unique_count[i] = st.size(); // 1
         }
 
         long long ans = 0;
-        for (long long i = 0; i < n; i++)
-            ans = max(ans, prefix_unique_count[i] + suffix_unique_count[i + 1]);
+        for (long long i = 0; i < n; i++) // n
+            ans = max(ans, prefix_unique_count[i] + suffix_unique_count[i + 1]); // 1
         cout << ans << endl;
     }
     return 0;
 }
+
+// tc = O(n) = O(2*10^5)
+// sc = O(n) = O(2*10^5)
