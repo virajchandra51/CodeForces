377281f96a7f2cdf0cf1f4c2512f0b3c796b7e30
Viraj Chandra <virajchandra51@gmail.com>
2025-03-18 22:29:34 +0530

    more codes

--- M_Minimum_LCM.cpp
+++ M_Minimum_LCM.cpp
@@ -3,27 +3,21 @@ using namespace std;
 
 int main()
 {
-    int t;
+    long long t;
     cin >> t;
     while (t--)
     {
-        int n;
+        long long n;
         cin >> n;
-        int l = INT_MAX;
-        int ans_a = 1;
-        int ans_b = n - 1;
-        for (int fac = 2; fac * fac <= n; fac++) // sqrtn
+        long long ans_a = 1;
+        long long ans_b = n - 1;
+        for (long long fac = 2; fac * fac <= n; fac++) // sqrt n
         {
             if (n % fac == 0)
             {
-                int a = n / fac;
-                int b = n - a;
-                if (l > lcm(a, b))
-                {
-                    l = lcm(a, b);
-                    ans_a = a;
-                    ans_b = b;
-                }
+                ans_a = n / fac;
+                ans_b = n - ans_a;
+                break;
             }
         }
         cout << ans_a << " " << ans_b << endl;
@@ -31,5 +25,5 @@ int main()
     return 0;
 }
 
-// tc - O(sqrt(10^9)) = O(10^4)
-// sc - O(1)
+// tc = O(sqrt(10^9)) = O(10^4)
+// sc = O(1)
