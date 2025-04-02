a41a58e22efcf9da79f672bb996818ba108478e0
Viraj Chandra <virajchandra51@gmail.com>
2025-04-03 01:33:21 +0530

    more codes

--- B_Beautiful_Array.cpp
+++ B_Beautiful_Array.cpp
@@ -9,30 +9,27 @@ int main()
     {
         long long n, k, b, s;
         cin >> n >> k >> b >> s;
-        long long y = (k * b);
-        if (y > s)
+        long long minimum_s = (k * b);
+        long long maximum_s = (k * b) + (k - 1) * n;
+        if(s < minimum_s || s > maximum_s)
             cout << "-1" << endl;
         else
         {
             vector<long long> ans(n, 0);
-            ans[0] = y;
-            s -= y;
-            for (int i = 0; i < n; i++)
+            ans[0] = minimum_s;
+            s -= minimum_s;
+            for (int i = 0; i < n; i++) // n
             {
                 long long add = min(k - 1, s);
                 ans[i] += add;
                 s -= add;
             }
-            if (s > 0)
-            {
-                cout << "-1" << endl;
-            }
-            else
-            {
-                for (long long i = 0; i < n; i++)
-                    cout << ans[i] << " ";
-                cout << endl;
-            }
+            for (long long i = 0; i < n; i++) // n
+                cout << ans[i] << " ";
+            cout << endl;
         }
     }
 }
+
+// tc = O(n) = O(10^5)
+// sc = O(n) = O(10^5)
--- example.cpp
+++ example.cpp
@@ -1,121 +1,81 @@
-class TrieNode
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
 {
-public:
-    // Initialize your data structure here.
-    int strings_ending_here;
-    int strings_going_below;
-    TrieNode *children[26];
-    TrieNode()
+    int t;
+    cin >> t;
+
+    while (t--)
     {
-        strings_ending_here = 0;
-        strings_going_below = 0;
-        for (int i = 0; i < 26; i++)
-            children[i] = NULL;
-    }
-};
+        long long n; // number of fence planks
+        int m;       // number of available colors
+        cin >> n >> m;
 
-class Trie{
-    public:
-    TrieNode *root;
-    Trie()
-    {
-        root = new TrieNode();
-    }
-    // add functions
-};
-
-// Adds a word into the trie.
-void add(string word)
-{
-    TrieNode *curr = root;
-    for (auto ch : word)
-    {
-        if (curr->children[ch - 'a'] == NULL)
-            curr->children[ch - 'a'] = new TrieNode();
-        curr = curr->children[ch - 'a'];
-        curr->strings_going_below++;
-    }
-    curr->strings_ending_here++;
-}
-
-// Searches a word in the trie.
-void search(string word)
-{
-    TrieNode *curr = root;
-    for (auto ch : word)
-    {
-        if (curr->children[ch - 'a'] == NULL)
-            return false;
-        curr = curr->children[ch - 'a'];
-    }
-    curr->strings_ending_here > 0;
-}
-
-
-const int letters = 26;
-struct TrieNode
-{
-    vector<int> children;
-    int strings_ending_here;
-    int strings_going_below;
-    TrieNode()
-    {
-        children.resize(letters, -1);
-        strings_ending_here = 0;
-        strings_going_below = 0;
-    }
-};
-
-struct Trie{
-    vector<Node> TrieTree;
-    int sizeOfTrie = 0;
-    Trie(){
-        TrieTree.push_back(Node());
-        sizeOfTrie++;
-    }
-    // add functions
-}
-
-void add(string word)
-{
-    int currNode = 0;
-    for(auto ch: word){
-        int index = ch-'a';
-        if(TrieTree[currNode].children[index] == -1){
-            TrieTree[currNode].children[index] = sizeOfTrie;
-            TrieTree.push_back(Node());
-            sizeOfTrie++;
+        vector<long long> a(m);
+        for (int i = 0; i < m; i++)
+        {
+            cin >> a[i];
         }
-        currNode = TrieTree[currNode].children[index];
-        TrieTree[currNode].strings_going_below++;
-    }
-    TrieTree[currNode].strings_ending_here++;
-}
+        // Compute L and R for each color.
+        vector<long long> L(m), R(m);
+        for (int i = 0; i < m; i++)
+        {
+            L[i] = min(a[i], n - 1LL);
+            R[i] = max(1LL, n - a[i]);
+        }
 
-bool search(string word)
-{
-    int currNode = 0;
-    for(auto ch: word){
-        int index = ch-'a';
-        if(TrieTree[currNode].children[index] == -1)
-            return false;
-        currNode = TrieTree[currNode].children[index];
-    }
-    return TrieTree[currNode].strings_ending_here > 0;
-}
+        // Sort copies of L and R for the "two-pointer" / prefix sum approach
+        vector<long long> X = L, Y = R;
+        sort(X.begin(), X.end());
+        sort(Y.begin(), Y.end());
 
-void delete(string word)
-{
-    if(search(word) == false)
-        return;
-    int currNode = 0;
-    for(auto ch: word){
-        int index = ch-'a';
-        int child = TrieTree[currNode].children[index];
-        TrieTree[child].strings_going_below--;
-        if(TrieTree[child].strings_going_below == 0)
-            TrieTree[currNode].children[index] = -1;
-        currNode = child;
+        // Prefix sum of Y for summing quickly
+        vector<long long> prefixY(m + 1, 0LL);
+        for (int i = 0; i < m; i++)
+        {
+            prefixY[i + 1] = prefixY[i] + Y[i];
+        }
+
+        // Sum of f(i,j) over all i, j (including i=j for now)
+        long long total = 0;
+        int j = 0; // pointer for Y
+        for (int i = 0; i < m; i++)
+        {
+            long long xVal = X[i];
+            // Move j so that Y[j] <= xVal
+            // i.e., we want the largest j such that Y[j] <= xVal
+            // We can do this with a while loop or binary search (upper_bound).
+            // Here we'll use binary search for clarity:
+            int p = int(upper_bound(Y.begin(), Y.end(), xVal) - Y.begin());
+            // p is the count of Y[j] <= xVal
+
+            if (p > 0)
+            {
+                // sum_{k=1..p} ( xVal - Y[k] + 1 )
+                // = p*(xVal + 1) - sum(Y[1..p])
+                long long sumOfY = prefixY[p]; // sum of first p Y's
+                total += p * (xVal + 1) - sumOfY;
+            }
+        }
+
+        // Now subtract the diagonal part f(i,i) for i=1..m
+        // f(i,i) = max( 0, L[i] - R[i] + 1 )
+        long long diag = 0;
+        for (int i = 0; i < m; i++)
+        {
+            long long val = L[i] - R[i] + 1;
+            if (val > 0)
+            {
+                diag += val;
+            }
+        }
+
+        // We only want i != j, so subtract any diagonal terms
+        long long answer = total - diag;
+
+        cout << answer << "\n";
     }
-    TrieTree[currNode].strings_ending_here--;
+
+    return 0;
 }
